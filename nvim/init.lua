d25148b3ddf7679a8d5ff2b6a8effc7e3b08fbcb
silasm01 <silasmikkelsen@outlook.com>
2024-03-11 13:04:34 +0100

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,7 +27,51 @@ require("lazy").setup({
     "williamboman/mason.nvim"
 },
 {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+},
+{
+	"hrsh7th/cmp-calc"
+},
+{
+	"hrsh7th/cmp-path"
+},
+{
+	'hrsh7th/cmp-nvim-lsp'
+},
+{
+	'hrsh7th/nvim-cmp',
+},
+{
 	"mhartington/formatter.nvim"
+},
+{
+	"tris203/hawtkeys.nvim",
+	dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = {}
+},
+--{
+--   "m4xshen/hardtime.nvim",
+--   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+--   opts = {}
+--},
+{
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+},
+{
+	'ThePrimeagen/vim-be-good'
 },
 --{
 --	"folke/lsp-colors.nvim"
@@ -52,15 +96,98 @@ require("lazy").setup({
 },
 {
 	"nvim-tree/nvim-tree.lua"
-}
+},
+{
+	"mbbill/undotree"
+},
+{
+	"nvim-treesitter/nvim-treesitter"
+},
+{
+	"folke/twilight.nvim"
+},
+{
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp"
+},
+{
+	'saadparwaiz1/cmp_luasnip'
+},
+{
+	"chrisgrieser/nvim-scissors",
+	dependencies = "nvim-telescope/telescope.nvim", -- optional
+	opts = {
+		snippetDir = "~/.config/snippets",
+	}
+},
+{
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+        require("copilot").setup({})
+  end,
+},
+{ "Gelio/cmp-natdat", config = true },
 }, opts)
 
 require("mason").setup()
 require("nvim-tree").setup()
 
-local lspconf = require("lspconfig")
-lspconf.rust_analyzer.setup {
+require("scissors").setup{
+	snippetDir = "~/.config/snippets"
 }
+
+local ls = require("luasnip")
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/snippets" } })
+local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      --{ name = 'vsnip' }, -- For vsnip users.
+       { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+      { name = "path" },
+      { name = "calc" },
+      { name = "natdat" }
+    })
+  })
+
+
+local lspconf = require("lspconfig")
+lspconf.rust_analyzer.setup {}
 lspconf.lua_ls.setup{}
 
+vim.keymap.set({"i", "s"}, "<Tab>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
+
 vim.cmd [[colorscheme catppuccin-macchiato]]
+
+vim.cmd [[set relativenumber]]
---
+++ spotify/Users/silasmikkelsen-user/frecency.pb
@@ -0,0 +1,31 @@
+
+>
+'spotify:playlist:5FhB6402y4g8fEe3icnwN6	ZîƒcÙ&y@ Ð­ ­
+>
+'spotify:playlist:0W3IHOZf8Bx6pb4Dk6wbyV	Û$½ÍU'y@ ª›¨­
+>
+'spotify:playlist:1u9WM7cJGDxk8sRNl1S7CM	DÉjæ_*y@ …­Ù­
+>
+'spotify:playlist:37i9dQZF1EIfwdv43q0LQH	.¼‰š`*y@ Ý²Ù­
+>
+'spotify:playlist:7tsj4w97qiwvEE7xxKpLcq	¨G³`*y@ Á³Ù­
+>
+'spotify:playlist:7IFT0oFaytRgOPD3vIEeP8	¯yÐ`*y@ ·´Ù­
+>
+'spotify:playlist:37i9dQZF1DXc1Etiiw8agT	·øÅo6y@ ë±•¯
+>
+'spotify:playlist:133TknMKs55ZMriDezIzG1	„’¶¡`*y@ ú²Ù­
+>
+'spotify:playlist:7GidzQHvNbnpnWTNTevSit	•“o•+y@ ‹Ðã­
+>
+'spotify:playlist:59uKT91zwFsgRIfkE2OJ0e	Ôæ°a*y@ †¶Ù­
+>
+'spotify:playlist:039LtyWa7HjiNFXEvaoKs0	|¦ÿµ¹.y@ ¾ØŸ®
+>
+'spotify:playlist:37i9dQZF1DZ06evO30usrj	HYòœP)y@ œÈ­
+>
+'spotify:playlist:37i9dQZF1EVKuMoAJjoTIw	åè™y5y@ ççŒ¯
+>
+'spotify:playlist:0h9Gaqt2sNJ8M5aMV3h9BO	¨ðã¸.y@ íÑŸ®
+>
+'spotify:playlist:0bkm9M8KSCsRIL7pjNW1OE	ëûÝ`*y@ ê´Ù­
